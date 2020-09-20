0187eaf8cf5dd338185083b2cc3bd4886a010729
Kristin239 <hkristin@andrew.cmu.edu>
2020-09-20 16:56:53 -0400

    wrote seed data for testing with

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,91 +10,91 @@ Town.create([
     {
         name: "Barnstable",
         location: "Mid Cape",
-        image: "./town-imgs/barnstable.jpg"
+        image: "./town-imgs/barnstable-img.jpg"
     },
 
     {
         name: "Bourne",
         location: "Upper Cape",
-        image: "./town-imgs/bourne.jpg"
+        image: "./town-imgs/bourne-img.jpeg"
     },
 
     {
         name: "Brewster",
         location: "Lower Cape",
-        image: "./town-imgs/brewster.jpg"
+        image: "./town-imgs/brewster-img.jpeg"
     },
 
     {
         name: "Chatham",
         location: "Lower Cape",
-        image: "./town-imgs/chatham.jpg"
+        image: "./town-imgs/chatham-img.jpg"
     },
 
     {
         name: "Dennis",
         location: "Mid Cape",
-        image: "./town-imgs/dennis.jpg"
+        image: "./town-imgs/dennis-img.jpeg"
     },
 
     {
         name: "Eastham",
         location: "Outer Cape",
-        image: "./town-imgs/eastham.jpg"
+        image: "./town-imgs/eastham-img.jpeg"
     },
 
     {
         name: "Falmouth",
         location: "Upper Cape",
-        image: "./town-imgs/falmouth.jpg"
+        image: "./town-imgs/falmouth-img.jpg"
     },
 
     {
         name: "Harwich",
         location: "Lower Cape",
-        image: "./town-imgs/harwich.jpg"
+        image: "./town-imgs/harwich-img.jpeg"
     },
 
     {
         name: "Mashpee",
         location: "Upper Cape",
-        image: "./town-imgs/mashpee.jpg"
+        image: "./town-imgs/mashpee-img.jpeg"
     },
 
     {
         name: "Orleans",
         location: "Lower Cape",
-        image: "./town-imgs/orleans.jpg"
+        image: "./town-imgs/orleans-img.jpeg"
     },
 
     {
         name: "Provincetown",
         location: "Outer Cape",
-        image: "./town-imgs/provincetown.jpg"
+        image: "./town-imgs/provincetown-img.jpeg"
     },
 
     {
         name: "Sandwich",
         location: "Upper Cape",
-        image: "./town-imgs/sandwich.jpg"
+        image: "./town-imgs/sandwich-img.jpeg"
     },
 
     {
         name: "Truro",
         location: "Outer Cape",
-        image: "./town-imgs/truro.jpg"
+        image: "./town-imgs/truro-img.jpeg"
     },
 
     {
         name: "Wellfleet",
         location: "Outer Cape",
-        image: "./town-imgs/wellfleet.jpg"
+        image: "./town-imgs/wellfleet-img.jpeg"
     },
 
     {
         name: "Yarmouth",
         location: "Mid Cape",
-        image: "./town-imgs/yarmouth.jpg"
+        image: "./town-imgs/yarmouth-img.jpeg"
     }
 ])
 
@@ -102,85 +102,85 @@ Beach.create([
     {   name: "Paine's Creek",
         length_of_beach: 2.5,
         image: "./beach-imgs/paines_creek.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Bass River Beach",
         length_of_beach: 3.5,
         image:"./beach-imgs/bass_river.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Cahoon Hollow Beach",
         length_of_beach: 3.5,
         image: "./beach-imgs/cahoon_hollow.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Craigsville Beach",
         length_of_beach: 2.5,
         image: "./beach-imgs/craigsville.jpg",
-        town_id: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Scusset Beach",
         length_of_beach: 3.5,
         image: "./beach-imgs/scusset.jpg",
-        town_id: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Mayflower Beach",
         length_of_beach: 3,
         image: "./beach-imgs/mayflower.jpg",
-        town_id: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Lighthouse Beach",
         length_of_beach: 4.5,
         image: "./beach-imgs/lighthouse.jpg",
-        town_id: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Coast Guard Beach",
         length_of_beach: 3.5,
         image: "./beach-imgs/coast_guard.jpg",
-        town_id: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Bristol Beach",
         length_of_beach: 3.5,
         image: "./beach-imgs/bristol.jpg",
-        town_id: Town.all.sample
+        town_id: Town.all.sample.id
     },
 
     {   name: "Ballston Beach",
         length_of_beach: 2.5,
         image: "./beach-imgs/ballston.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
     {   name: "Race Point",
         length_of_beach: 5.5,
         image: "./beach-imgs/race_point.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
     {   name: "Horizon's Beach",
         length_of_beach: 3.5,
         image: "./beach-imgs/horizons.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
-    {   name: "Nauset",
+    {   name: "Nauset Beach",
         length_of_beach: 6.5,
         image: "./beach-imgs/nauset.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
     {   name: "Red River Beach",
         length_of_beach: 2.5,
         image: "./beach-imgs/red_river.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     },
     {   name: "Attaquin Park Beach",
         length_of_beach: 4.5,
         image: "./beach-imgs/attaquin_park.jpg",
-        town: Town.all.sample
+        town_id: Town.all.sample.id
     }   
 ])
